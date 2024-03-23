69052d106eb77104be36be5d386d03d5489fd194
Seohyeon.Lim <forestlight749@gmail.com>
2024-03-23 19:31:58 +0900

    회원 등록

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,10 +7,30 @@ public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        // 여기서 EntityManager를 꺼내고
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        // transaction
+        EntityTransaction tx = em.getTransaction(); // tx 얻기
+        tx.begin(); // tx 시작
+
+        try {
+            // EntityManager안에서 실제 동작하는 code는 여기에 작성
+            Member member = new Member();
+            // 멤버의 아이디와 이름 설정
+            member.setId(1L);
+            member.setName("HelloA");
+
+            em.persist(member); // 멤버 넣기(저장)
+
+            tx.commit(); // 정상적일 때 저장, 문제가 생기면 Rollback(철회)
+        } catch (Exception e) {
+            tx.rollback(); // 문제가 생기면 철회
+        } finally {
+            em.close(); // 작업이 끝나면 EntityManager를 닫아줌.
+        }
+
+        // 전체 애플리케이션이 완전히 끝나면 EntityManagerFactory를 닫아주어야 함.
         emf.close();
     }
 }
---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,31 @@
+package hellojpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity  // 이걸 넣어줘야 JPA를 처음 로딩할 때, JPA를 사용하는 클래스라는 것을 인지하고 관리함.
+// DB와 매핑되는 Member 클래스
+public class Member {
+
+    @Id
+    private Long id;
+    private String name;
+
+    // 필요한 Getter와 Setter 생성
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
